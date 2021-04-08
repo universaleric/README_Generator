d8217c6791b1e4eeb035ca13ac2158789bb56e2f
Eric Hay <ericdanielhay@yahoo.com>
2021-04-07 19:49:13 -0500

    Created .then(response)

---
+++ README.md
@@ -0,0 +1,67 @@
+# README Generator
+    
+    ### Description
+
+    This is an application to help the user quickly generate a README file.
+
+    ## Table of Contents
+
+    [About This Project](#about)
+    [Installation](#installation)
+    [Usage](#usage)
+    [Roadmap](#features)
+    [Contributing](#contributing)
+    [License](#license)
+    [Contact](#contact)
+    [Acknowledgements](#acknowledgement)
+
+    <a name="about"></a>
+    ## About This Project
+
+    It can be a headache trying to remember everything you need to include in a README file for a project. Trying to format a README can also be time consuming. This application is designed to cut down on that complexity and make a professional README.
+
+    ### Built With
+
+    Javascript
+
+    <a name="installation"></a>
+    ## Installation
+
+    npm inquirer
+
+    <a name="usage"></a>
+    ## Usage
+
+    This project can be used anytime a user needs to generate a new README file.
+
+    <a name="features"></a>
+    ## Roadmap
+
+    This project will work to stay up to date with current README formats.
+
+    <a name="contributing"></a>
+    ## Contributing
+
+    Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+    1. Fork the Project
+    2. Create your Feature Branch (git checkout -b feature/AmazingFeature)
+    3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+    4. Push to the Branch (git push origin feature/AmazingFeature)
+    5. Open a Pull Request
+
+    <a name="license"></a>
+    ## License
+
+    This project is licensed under MIT license.
+
+    <a name="contact"></a>
+    ## Contact
+
+    Eric Hay - ericdanielhay@yahoo.com
+    Link to project: https://github.com/universaleric/README_Generator
+
+    <a name="acknowledgement"></a>
+    ## Acknowledgements
+
+    Othneil Drew - https://github.com/othneildrew
---
+++ index.js
@@ -0,0 +1,141 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'What is a short description of your project?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What is your project about?',
+      name: 'about',
+    },
+    {
+      type: 'input',
+      message: 'What was your project built with?',
+      name: 'built',
+    },
+    {
+      type: 'input',
+      message: 'What dependencies are necessary to install?',
+      name: 'dependencies',
+    },
+    {
+      type: 'input',
+      message: 'How can this project be used?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'What are the proposed features for this project?',
+      name: 'features',
+    },
+    {
+      type: 'input',
+      message: 'What license is this project licensed under?',
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is your email?',
+      name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the link to this project?',
+        name: 'link',
+    },
+    {
+        type: 'input',
+        message: 'Who would you like to acknowledge?',
+        name: 'acknowledgements',
+    },
+  ])
+  .then((response) => {
+    let readmeString = `# ${response.title}
+    
+    ### Description
+
+    ${response.description}
+
+    ## Table of Contents
+
+    [About This Project](#about)
+    [Installation](#installation)
+    [Usage](#usage)
+    [Roadmap](#features)
+    [Contributing](#contributing)
+    [License](#license)
+    [Contact](#contact)
+    [Acknowledgements](#acknowledgement)
+
+    <a name="about"></a>
+    ## About This Project
+
+    ${response.about}
+
+    ### Built With
+
+    ${response.built}
+
+    <a name="installation"></a>
+    ## Installation
+
+    ${response.dependencies}
+
+    <a name="usage"></a>
+    ## Usage
+
+    ${response.usage}
+
+    <a name="features"></a>
+    ## Roadmap
+
+    ${response.features}
+
+    <a name="contributing"></a>
+    ## Contributing
+
+    Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+    1. Fork the Project
+    2. Create your Feature Branch (git checkout -b feature/AmazingFeature)
+    3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+    4. Push to the Branch (git push origin feature/AmazingFeature)
+    5. Open a Pull Request
+
+    <a name="license"></a>
+    ## License
+
+    This project is licensed under ${response.license}.
+
+    <a name="contact"></a>
+    ## Contact
+
+    ${response.name} - ${response.email}
+    Link to project: ${response.link}
+
+    <a name="acknowledgement"></a>
+    ## Acknowledgements
+
+    ${response.acknowledgements}`;
+
+
+
+    fs.writeFile('README.md', readmeString, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  )}
+    );
